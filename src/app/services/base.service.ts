7d814dfdd5238714e76ea8df829853d5bc953f65
yskanth2019 <yskanth2019@gmail.com>
2019-09-19 05:41:08 -0700

    first commit

---
+++ src/app/services/base.service.ts
@@ -0,0 +1,32 @@
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import { ServiceError } from '../models/serviceerror';
+
+
+export class BaseService {
+    constructor() { }
+    protected extractData(res: Response) {
+        const body = res.json();
+        if (body.status === 'success') {
+            return body.data;
+        } else if (body.status === 'fail') {
+            throw new ServiceError(body.message, body.data, 'fail');
+        } else if (body.status === 'error') {
+            throw new ServiceError(body.message, body.data);
+        } else {
+            throw new ServiceError('Invalid JSend Response Status [' + body.status + ']');
+        }
+    }
+    public baseurl(): string {
+        return 'http://localhost:50830/';
+    }
+
+    protected handleError(error: any) {
+        if (error instanceof ServiceError) {
+            return Observable.throw(error);
+        } else {
+            const errMsg = (error.message) ? error.message : error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+            return Observable.throw(new ServiceError(errMsg));
+        }
+    }
+}
---
+++ src/app/services/event.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class EventService {
+
+  constructor(private toastr: ToastrService) { }
+
+  showSuccess(msg: string) {
+    this.toastr.success(msg);
+  }
+
+  showWarning(msg: string) {
+    this.toastr.warning(msg);
+  }
+
+  showError(msg: string) {
+    this.toastr.error(msg);
+  }
+
+  showLoading(show: boolean) {
+    if (show)
+      document.getElementById('loading-div').style.display = 'block';
+    else
+      document.getElementById('loading-div').style.display = 'none';
+  }
+}
